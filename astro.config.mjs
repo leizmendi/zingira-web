ba5eedccffcace20d36e368d0ab33e4669eff892
leizmendi <leizmendi@gmail.com>
2026-04-05 23:42:29 +0200

    Añadido soporte para redirecciones en la configuración de Astro para el idioma español.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+  redirects: {
+    '/es': '/',
+    '/es/[...slug]': '/[...slug]',
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'eu', 'en', 'fr'],
